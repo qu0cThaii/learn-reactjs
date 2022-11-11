81a30a4a899bc9fdf1720a0e689ca2c8902df94d
qu0cthaii <quocthai034@gmail.com>
2022-11-11 17:16:19 +0700

    app title

--- src/App.js
+++ src/App.js
@@ -2,24 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to quocthai
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          qu0cThaii
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">quocthai</div>;
 }
 
 export default App;
